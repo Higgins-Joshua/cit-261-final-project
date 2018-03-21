49d4de6ca17f8d6e29222093d33ae0cf73e74f05
Joshua-Higgins <joshua.higgins@rakuten.com>
2018-03-20 22:13:02 -0600

    added api url

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const urlencodedParser = bodyParser.urlencoded({extended: true})
 const PORT = process.env.PORT || 5000
+const API_KEY = '8b847caf'
+const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&t=`
 
 var app = express()
 
@@ -10,6 +12,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => res.render('pages/index'))
+app.get('/', (req, res) => res.render('pages/index', {apiUrl: API_URL}))
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
