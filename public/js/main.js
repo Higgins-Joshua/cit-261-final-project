55028ca00db06240c3e0b4c9daeab7975ac85db9
Joshua-Higgins <joshua.higgins@rakuten.com>
2018-03-29 20:33:22 -0600

    showing on page

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,25 @@ function searchDB(event, url) {
         })
 
         .then(function(json) {
-            console.log(json);
+            // console.log(json.Ratings.map(source => console.log(source.Source + ' ' + source.Value)));
+            populateMovieData(json);
         })
+
+    event.movieTitle.value = '';
+}
+
+function populateMovieData(movieObj) {
+  document.querySelector('#titleOfMovie').innerHTML = movieObj.Title;
+  document.querySelector('#releasedDate').innerHTML = movieObj.Released;
+  document.querySelector('#movieDirector').innerHTML = movieObj.Director;
+  document.querySelector('#production').innerHTML = movieObj.Production;
+  document.querySelector('#genre').innerHTML = movieObj.Genre;
+  document.querySelector('#movieImage').src = movieObj.Poster;
+
+  // document.querySelector('#movieRatings').innerHTML =
+  movieObj.Ratings.map(rating =>
+    document.querySelector('#movieRatings').innerHTML += (`<li>${rating.Source}: ${rating.Value}</li>`)
+  );
+
+  console.log(movieObj);
 }
