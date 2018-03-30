0cd9025ff28516c7bc0db607b57633f666561163
Joshua-Higgins <joshua.higgins@rakuten.com>
2018-03-29 20:39:44 -0600

    fixed rating bug

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ const API_KEY = '8b847caf'
 const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`
 
 function searchDB(event, url) {
+    document.querySelector('#movieRatings').innerHTML = '';
     fetch(`${API_URL}&t=${event.movieTitle.value}`)
         .then(function(data){
             return data.json();
